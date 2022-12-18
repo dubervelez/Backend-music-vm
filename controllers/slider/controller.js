e0f7186d4e00001405733aec3487e7632dba399a
Duberney Velez <duberneyvelez11@gmail.com>
2022-12-17 23:29:31 -0500

    modificando filtro para el metodo PATH del controlador del slider

--- controllers/slider/controller.js
+++ controllers/slider/controller.js
@@ -1,14 +1,24 @@
 import { getDB } from '../../db/basedatos.js';
+import { ObjectId } from 'mongodb';
 
 const queryallslider = async (callback)=>{
     const conexion = getDB(); 
     await conexion.collection('slider').find({}).limit(50).toArray(callback);
 } 
 
-const crearSlider = (datosSlider, callback)=>{
+const crearSlider = async(datosSlider, callback)=>{
     const conexion = getDB();
-    conexion.collection('slider').insertOne(datosSlider, callback)
+    await conexion.collection('slider').insertOne(datosSlider, callback)
+}
+
+const actualizarSlider = async(edicion, callback)=>{
+    const operacion = {
+        $set:edicion
+    }
+    const idEdicion = { idSlider: edicion.idSlider }
+    const conexion = getDB();
+    await conexion.collection('slider').findOneAndUpdate(idEdicion, operacion, callback)
 }
 
 
-export { queryallslider, crearSlider };
+export { queryallslider, crearSlider, actualizarSlider };
--- views/slider/rutas.js
+++ views/slider/rutas.js
@@ -1,7 +1,7 @@
 import Express  from 'express';
 import { getDB } from '../../db/basedatos.js';
-import { ObjectId } from 'mongodb';
-import { queryallslider, crearSlider } from '../../controllers/slider/controller.js';
+
+import { queryallslider, crearSlider, actualizarSlider } from '../../controllers/slider/controller.js';
 
 const rutasSlider = Express.Router();
 
@@ -25,27 +25,13 @@ rutasSlider.route("/admin/informacion-slider").get ( (req, res)=>{
 })
     
 rutasSlider.route("/admin/creacion-slider").post((req, res)=>{
-    console.log("el usuario envio el requerimiento"+ req.body)
-    crearSlider(req.body, genericCallback(res))
+    console.log("el usuario envio el requerimiento"+ req.body);
+    crearSlider(req.body, genericCallback(res));
 })
 
 rutasSlider.route('/admin/modificar-slider').patch((req, res)=>{
-    const edicion = req.body;
-    const operacion = {
-        $set:edicion
-    }
-    const idEdicion = { _id: new ObjectId(edicion.id) }
-    delete edicion.id;
-    const conexion = getDB();
-    conexion.collection('slider').findOneAndUpdate(idEdicion, operacion, (err, result)=>{
-        if (err) {
-            console.error('error actualizando el slider')
-            res.sendStatus(500)
-        }else{
-            console.log('slider actualizado')
-            res.sendStatus(200)
-        }
-    })
-})
+    console.log("el usuario quiere editar la db slider");
+    actualizarSlider(req.body, genericCallback(res));
+});
 
 export default rutasSlider;
