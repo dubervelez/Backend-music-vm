ab1f72fb13e7e6a08b3ee992a7d7c9ede403c17b
Duberney Velez <duberneyvelez11@gmail.com>
2023-01-09 11:17:52 -0500

    cambio de configuracion para hacer deploy en railway

--- db/basedatos.js
+++ db/basedatos.js
@@ -3,7 +3,7 @@ import Dotenv from "dotenv";
 
 Dotenv.config({ path: './.env' });
 
-const url = process.env.DATABASE_URL;
+const url = process.env.DATABASE_URL || 'mongodb+srv://DuberVelez:Codeba1129535664@proyecto-music-vm.1zorlpc.mongodb.net/?retryWrites=true&w=majority';
 const client = new MongoClient(url, {
     useNewUrlParser: true,
     useUnifiedTopology:true
